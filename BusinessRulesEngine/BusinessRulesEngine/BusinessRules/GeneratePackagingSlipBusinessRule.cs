9cfbba5b7dec0026ea055b54bdf4f1f0b8d7d915
Ynoaparast <Ynoaparast@users.noreply.github.com>
2021-08-15 18:01:21 +0200

    - updated method to take parameter of type Product instead of IProduct

--- BusinessRulesEngine/BusinessRulesEngine/BusinessRules/GeneratePackagingSlipBusinessRule.cs
+++ BusinessRulesEngine/BusinessRulesEngine/BusinessRules/GeneratePackagingSlipBusinessRule.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using BusinessRulesEngine.Interfaces;
+using BusinessRulesEngine.Models;
 
 namespace BusinessRulesEngine.BusinessRules
 {
     public class GeneratePackagingSlipBusinessRule : IBusinessRule
     {
         public string SlipDestination { get; set; }
-        public void ExecuteBusinessRule(IProduct product)
+        public void ExecuteBusinessRule(Product product)
         {
             throw new NotImplementedException();
         }
