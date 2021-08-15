17f958a289ec30fb3781ff601495bd5aa5eb5477
Ynoaparast <Ynoaparast@users.noreply.github.com>
2021-08-15 18:21:34 +0200

    - Updated payment handler to check for member type when the product is Book, instead of checking the value of the ProductType property. - All tests pass again

--- BusinessRulesEngine/BusinessRulesEngine/PaymentHandler.cs
+++ BusinessRulesEngine/BusinessRulesEngine/PaymentHandler.cs
@@ -23,12 +23,12 @@ namespace BusinessRulesEngine
               payment.BusinessRules.Add(new GeneratePackagingSlipBusinessRule(){SlipDestination = "Customer"});
             }
 
-            if (payment.Product.ProductType == "Book")
+            if (payment.Product.GetType() == typeof(Book))
             {
                 payment.BusinessRules.Add(new GeneratePackagingSlipBusinessRule(){SlipDestination = "Royalty"});
             }
 
-            if (payment.Product.IsPhysical || payment.Product.ProductType == "Book")
+            if (payment.Product.IsPhysical || payment.Product.GetType() == typeof(Book))
             {
                 payment.BusinessRules.Add(new GenerateAgentCommissionBusinessRule());
             }
