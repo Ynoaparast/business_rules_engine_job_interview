f0c2e5e337fd78876202913851bf67566877850e
Ynoaparast <Ynoaparast@users.noreply.github.com>
2021-08-15 19:31:30 +0200

    - Updated SendEmailForMemberShipBusinessRule to hold Email and Customer information - Added the method CreateEmailToSend that creates a dummy email to send

--- BusinessRulesEngine/BusinessRulesEngine/BusinessRules/SendEmailForMembershipBusinessRule.cs
+++ BusinessRulesEngine/BusinessRulesEngine/BusinessRules/SendEmailForMembershipBusinessRule.cs
@@ -8,9 +8,32 @@ namespace BusinessRulesEngine.BusinessRules
 {
     public class SendEmailForMembershipBusinessRule : IBusinessRule
     {
+        public Email Email { get; set; }
+        public Customer Customer { get; set; }
+
+        public SendEmailForMembershipBusinessRule(Customer customer)
+        {
+            Customer = customer;
+        }
+
+        public void CreateEmailToSend(Product product)
+        {
+            var productText = product.GetType() == typeof(Membership) ? "membership has been activated" : "membership has been upgraded";
+            var emailContent = $"Dear {Customer.Firstname} {Customer.Lastname}, your {productText}";
+            Email = new Email()
+            {
+                Content = emailContent,
+                To = Customer.Email
+            };
+   
+
+        }
+
         public void ExecuteBusinessRule(Product product)
         {
-           
+            CreateEmailToSend(product);
+
+            //...Send Email
         }
     }
 }
