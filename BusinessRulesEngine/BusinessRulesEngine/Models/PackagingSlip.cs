be21145cd992446ea62aeb4741b0a16f5af684ee
Ynoaparast <Ynoaparast@users.noreply.github.com>
2021-08-15 20:37:20 +0200

    - Added PackagingSlip class

---
+++ BusinessRulesEngine/BusinessRulesEngine/Models/PackagingSlip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRulesEngine.Models
+{
+    public class PackagingSlip
+    {
+        public List<Product> ProductsToPack { get; set; }
+        public string SlipDestination { get; set; }
+
+        public PackagingSlip(string slipDestination)
+        {
+            SlipDestination = slipDestination;
+            ProductsToPack = new List<Product>();
+        }
+
+    }
+}
