615ae457498d832d502a047acdc159098aeb15f6
Ynoaparast <Ynoaparast@users.noreply.github.com>
2021-08-15 18:18:59 +0200

    - Added Book class - Added MembershipUpgrade class - Added UpgradeMembershipBusinessRule

---
+++ BusinessRulesEngine/BusinessRulesEngine/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRulesEngine.Models
+{
+    public class Book : Product
+    {
+        
+    }
+}
---
+++ BusinessRulesEngine/BusinessRulesEngine/Models/MembershipUpgrade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessRulesEngine.Interfaces;
+
+namespace BusinessRulesEngine.Models
+{
+    public class MembershipUpgrade : Product
+    {
+        public Membership Membership { get; set; }
+    }
+}
