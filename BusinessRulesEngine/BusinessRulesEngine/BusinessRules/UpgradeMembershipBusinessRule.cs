d526a9d732a78d1d896aeaea868d774c12b197fb
Ynoaparast <Ynoaparast@users.noreply.github.com>
2021-08-15 18:19:23 +0200

    - Added UpgradeMembershipBusinessRule class (missing in previous commit by mistake)

---
+++ BusinessRulesEngine/BusinessRulesEngine/BusinessRules/UpgradeMembershipBusinessRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessRulesEngine.Interfaces;
+using BusinessRulesEngine.Models;
+
+namespace BusinessRulesEngine.BusinessRules
+{
+    public class UpgradeMembershipBusinessRule : IBusinessRule
+    {
+        public void ExecuteBusinessRule(Product product)
+        {
+            var membership = (Membership) product;
+            membership.IsUpgraded = true;
+        }
+    }
+}
