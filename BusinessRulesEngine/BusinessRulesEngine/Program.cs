c375543610286bcdffc2b01774666f4bc1b1291b
Ynoaparast <Ynoaparast@users.noreply.github.com>
2021-08-15 11:16:56 +0200

    Initial commit - Added main .NET Core console project BussinesRulesEngine - Added NUnit test project Test_BusinessRulesEngine - Added FluentAssertions NuGet-package

---
+++ BusinessRulesEngine/BusinessRulesEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessRulesEngine
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
